d2e649ba84ac2f7f6ca8508a91fc10d4bbe66bc9
kamip <kamip.net@gmail.com>
2014-09-29 19:47:24 +0900

    sorya-

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestWercker::Application.config.secret_token = '67061cfa6d2e489cbee649029009c02c8b3c5d2d52b1a757979e3963b39b281a9c2b541cc97e2581d57b7a2228f72f5e3249c53a111f5ebf4553d835a2bc04d4'
